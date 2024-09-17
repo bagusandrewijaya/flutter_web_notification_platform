dccdc9b3e78cfb3aa6a69f52d0f218a692391c36
Bagus Andre Wijaya <103176578+bagusandrewijaya@users.noreply.github.com>
2024-09-17 23:12:32 +0700

    rewwork function

---
+++ example/main.dart
@@ -0,0 +1,31 @@
+// example/main.dart
+import 'package:flutter/material.dart';
+import 'package:flutter_web_notification_platform/platformNotification.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final PlatformNotification platformNotification = PlatformNotificationWeb();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Web Push Notifications Example'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              platformNotification.requestPermission();
+              platformNotification.sendNotification('Hello!', 'This is a test notification.');
+            },
+            child: Text('Send Notification'),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_web_notification_platform/src/platformNotification_stub.dart'
-    if (dart.library.html)  'package:flutter_web_notification_platform/services/platformNotification.dart';
