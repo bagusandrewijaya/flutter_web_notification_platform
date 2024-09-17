85dd1d9e0648321fbbb0c4c512b364d32b70b9dd
Bagus Andre Wijaya <103176578+bagusandrewijaya@users.noreply.github.com>
2024-09-17 23:47:30 +0700

    rename files to snake cases bcs make issued

--- lib/flutter_web_notification_platform.dart
+++ lib/flutter_web_notification_platform.dart
@@ -1,6 +1,6 @@
 
 export 'src/platform_notification_stub.dart'
-    if (dart.library.html) 'src/platformNotification_web.dart';
+    if (dart.library.html) 'src/platform_notification_web.dart';
 
 abstract class PlatformNotification {
   void sendNotification(String title, String body);
